5b274a3d68185b7b06830e91ccbaf4ec2cb81712
Emma Edvardsson <emmed608@student.liu.se>
2015-04-14 13:47:13 +0200

    Hittar Metaioman i om man växlar till CameraView

--- Projekt/app/src/main/java/com/example/viktor/agilprojektaugmentedreality/CameraActivity.java
+++ Projekt/app/src/main/java/com/example/viktor/agilprojektaugmentedreality/CameraActivity.java
@@ -1,14 +1,38 @@
 package com.example.viktor.agilprojektaugmentedreality;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+import com.metaio.sdk.ARELActivity;
 import com.metaio.sdk.ARViewActivity;
+import com.metaio.sdk.MetaioDebug;
 import com.metaio.sdk.jni.IGeometry;
 import com.metaio.sdk.jni.IMetaioSDKCallback;
+import com.metaio.tools.io.AssetsManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
 
 
 public class CameraActivity extends ARViewActivity {
+
+    WebView mWebView;
+
+    /**
+     * Progress view
+     */
+    View mProgress;
 
     @Override
     protected int getGUILayout() {
@@ -22,7 +46,47 @@ public class CameraActivity extends ARViewActivity {
 
     @Override
     protected void loadContents() {
+        try
+        {
+            //Lade till denna try/catch från AssetsExtracter/doInBackground i TemplateAREL/MainActivity = MAGI!!!
+            try
+            {
+                // Extract all assets and overwrite existing files if debug build
+                AssetsManager.extractAllAssets(getApplicationContext(), BuildConfig.DEBUG);
+            }
+            catch (IOException e)
+            {
+                MetaioDebug.log(Log.ERROR, "Error extracting assets: "+e.getMessage());
+                MetaioDebug.printStackTrace(Log.ERROR, e);
+            }
 
+            //Kopierat från TutorialHelloWorld.java
+            // Getting a file path for tracking configuration XML file
+            File trackingConfigFile = AssetsManager.getAssetPathAsFile(getApplicationContext(), "TutorialHelloWorld/Assets/TrackingData_MarkerlessFast.xml");
+
+            // Assigning tracking configuration
+            boolean result = metaioSDK.setTrackingConfiguration(trackingConfigFile);
+            MetaioDebug.log("Tracking data loaded: " + result);
+
+            // Getting a file path for a 3D geometry
+            File metaioManModel = AssetsManager.getAssetPathAsFile(getApplicationContext(), "TutorialHelloWorld/Assets/metaioman.md2");
+            if (metaioManModel != null)
+            {
+                // Loading 3D geometry
+                IGeometry geometry = metaioSDK.createGeometry(metaioManModel);
+                if (geometry != null)
+                {
+                    // Set geometry properties
+                    geometry.setScale(4f);
+                }
+                else
+                    MetaioDebug.log(Log.ERROR, "Error loading geometry: "+metaioManModel);
+            }
+        }
+        catch (Exception e)
+        {
+            MetaioDebug.printStackTrace(Log.ERROR, e);
+        }
     }
 
     @Override
@@ -58,4 +122,5 @@ public class CameraActivity extends ARViewActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
